b7c8f249a459e662735efcade1704c80e4837fe6
[Gravity] Toan Nguyen <toan.nguyenba@axonactive.com>
2020-06-20 14:19:28 +0700

    add listener for canvas

--- src/components/home/HomeCanvas.js
+++ src/components/home/HomeCanvas.js
@@ -16,7 +16,7 @@ export const buildHomeCanvas = (homeCanvasRef, space) => {
     let count = clientWidth * 0.05;
     if (count > 150) count = 150;
     const line = new Group (new Pt(0, angle), new Pt(clientWidth, 0));
-    const mouse = center.clone();
+    let mouse = new Pt(0, 0);
 
     for (let i = 0; i < count; i++) {
         const p = new Pt(Math.random() * clientWidth, Math.random() * clientHeight);
@@ -32,26 +32,33 @@ export const buildHomeCanvas = (homeCanvasRef, space) => {
                 // rotate the points slowly
                 const pt = pts[i];
 
-                pt.rotate2D(0.0005, center);
+                pt.rotate2D(0.001, center);
                 form.stroke(false).fill(colors[i % 3]).point(pt, 1);
 
-                // get line from pt to the mouse line
+                // get line from pt to the center line
                 const ln = new Group(pt, Line.perpendicularFromPt(line, pt));
 
                 // opacity of line derived from distance to the line
-                const distFromMouse = Math.abs(Line.distanceFromPt(ln, mouse));
+                const distFromCenter = Math.abs(Line.distanceFromPt(ln, center));
 
-                if (distFromMouse < 80) {
-                    if (pts[i].brightness < 0.3) pts[i].brightness += 0.015
+                if(Line.collinear(ln[0], ln[1], mouse, 1.5)){
+                    pts[i].brightness = 0.3;
                 } else {
-                    if (pts[i].brightness > 0.1) pts[i].brightness -= 0.01
+                    if (distFromCenter < 80) {
+                        if (pts[i].brightness < 0.3) pts[i].brightness += 0.015
+                    } else {
+                        if (pts[i].brightness > 0.1) pts[i].brightness -= 0.01
+                    }
                 }
 
                 const color = "rgba(255,255,255," + pts[i].brightness + ")";
-
                 form.stroke(color).fill(true).line(ln);
             }
         }
+    });
+
+    space._canvas.addEventListener('pointermove', function (e) {
+        mouse = new Pt(e.offsetX, e.offsetY);
     });
 
     space.play();
