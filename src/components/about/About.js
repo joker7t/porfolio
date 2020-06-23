d8f8ce267917fcc786d307f9464a57cdaf59ccea
joker7t <batoan131196@gmail.com>
2020-06-23 23:00:11 +0700

    add postion sticky for header

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,12 +3,15 @@ import classes from './css/About.module.scss';
 import Header from './Header';
 
 const About = () => {
+
     return (
-        <div style={{ minHeight: '100vh' }}>
+        <div style={{ minHeight: '200vh' }}>
+            <p>Lorem ipsum...</p>
             <Header />
-            about
+            <p>Lorem ipsum...</p>
         </div>
     );
+
 }
 
 export default About;
--- src/components/about/Header.js
+++ src/components/about/Header.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classes from './css/Header.module.scss';
 
 const Header = () => {
+    const [isSticky, setSticky] = useState(false);
+    const ref = useRef(null);
+
+    const handleScroll = () => {
+        if (ref.current) {
+            setSticky(ref.current.getBoundingClientRect().top <= 0);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', () => handleScroll);
+        };
+    }, []);
+
     return (
-        <div>
-            <h1 className="sticky-inner">Sticky</h1>;
+        <div className={`${classes.stickyWrapper} ${isSticky ? classes.sticky : ''}`} ref={ref}>
+            <h1 className={classes.stickyInner}>Sticky</h1>
         </div>
     );
 }
