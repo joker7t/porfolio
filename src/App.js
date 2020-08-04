d73d930d39ae7d3a363e5f1b5d99cc49456845ef
joker7t <batoan131196@gmail.com>
2020-08-04 22:50:02 +0700

    fix header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './resources/css/App.scss';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Landing from './components/Landing';
+import './App.scss';
+import Header from './components/about/Header';
 
 function App() {
   return (
     <Router>
       <div>
+        <Header />
         <Switch>
           <Route exact path='/' component={Landing} />
           <Redirect from="*" to="/" />
