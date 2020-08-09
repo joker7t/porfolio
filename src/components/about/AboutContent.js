3cbc10ccffe25e5ff8065d14ccb0481165274f83
joker7t <batoan131196@gmail.com>
2020-08-09 12:22:08 +0700

    change color

--- src/components/about/AboutContent.js
+++ src/components/about/AboutContent.js
@@ -8,7 +8,7 @@ const AboutContent = () => {
         <div>
             <Fade right>
                 <div className={style.AboutContent} >
-                    I’m a Web developer in Axon Active Vietnam <br />
+                    I’m a Web developer in Axon Active Vietnam. <br />
                     I have over 3 years of developing web applications. I am working as a Full-stack developer for projects about fintech and e-commerce. My experience includes MySql, Postgres, MongoDB, Java, JSF, Spring, Javascript, React, Nginx. I also have experience in testing.<br />
                     Besides that, I am familiar with Kanban & Scrum process. More important, I’m confident, hard-working and interested in learning new things.
                 </div>
