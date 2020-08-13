79787582ab1f8d9d83b700f80ec3f66b44f31232
joker7t <batoan131196@gmail.com>
2020-08-13 22:53:02 +0700

    fix skill chart

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,6 +4,7 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import * as am4plugins_wordCloud from "@amcharts/amcharts4/plugins/wordCloud";
 import data from './skills-data';
 import Heading from '../util/Heading';
+import SkillProgressBar from './SkillProgressBar';
 import { Container, Row, Col } from "react-bootstrap";
 import Reveal from 'react-reveal/Reveal';
 import style from './css/Skills.module.scss';
@@ -11,34 +12,34 @@ import style from './css/Skills.module.scss';
 const Skills = () => {
 
     const show = () => {
-        am4core.ready(function () {
+        setTimeout(() => {
+            am4core.ready(function () {
+                // Themes begin
+                am4core.useTheme(am4themes_animated);
+                // Themes end
 
-            // Themes begin
-            am4core.useTheme(am4themes_animated);
-            // Themes end
+                const chart = am4core.create("skill-chart", am4plugins_wordCloud.WordCloud);
+                chart.fontFamily = "Courier New";
+                const series = chart.series.push(new am4plugins_wordCloud.WordCloudSeries());
+                series.randomness = 0.1;
+                series.rotationThreshold = 0.5;
+                series.data = data;
 
-            var chart = am4core.create("skill-chart", am4plugins_wordCloud.WordCloud);
-            chart.fontFamily = "Courier New";
-            var series = chart.series.push(new am4plugins_wordCloud.WordCloudSeries());
-            series.randomness = 0.1;
-            series.rotationThreshold = 0.5;
-            series.data = data;
+                series.dataFields.word = "tag";
+                series.dataFields.value = "count";
 
-            series.dataFields.word = "tag";
-            series.dataFields.value = "count";
+                series.heatRules.push({
+                    "target": series.labels.template,
+                    "property": "fill",
+                    "min": am4core.color("#0000CC"),
+                    "max": am4core.color("#CC00CC"),
+                    "dataField": "value"
+                });
+                const hoverState = series.labels.template.states.create("hover");
+                hoverState.properties.fill = am4core.color("#C98D4B");
 
-            series.heatRules.push({
-                "target": series.labels.template,
-                "property": "fill",
-                "min": am4core.color("#0000CC"),
-                "max": am4core.color("#CC00CC"),
-                "dataField": "value"
             });
-
-            var hoverState = series.labels.template.states.create("hover");
-            hoverState.properties.fill = am4core.color("#C98D4B");
-
-        });
+        }, 500);
     }
 
     return (
@@ -50,8 +51,8 @@ const Skills = () => {
                 <Reveal onReveal={() => show()}>
                     <Row className={style.Container}>
                         <Col lg={7} className={style.Left}>
-                            sdfdsfdsfds
-                    </Col>
+                            <SkillProgressBar />
+                        </Col>
                         <Col lg={5} className={style.Right} style={{ padding: '0' }} id='skill-chart'>
                         </Col>
                     </Row>
