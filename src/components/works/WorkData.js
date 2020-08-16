8c391351b3909f9f2ab3d7973261bb20aeaa78fb
joker7t <batoan131196@gmail.com>
2020-08-16 14:57:23 +0700

    add to picture to data

--- src/components/works/WorkData.js
+++ src/components/works/WorkData.js
@@ -1,30 +1,75 @@
+import Manc1 from '../../resources/images/works/man-city/image-1.png';
+import Manc2 from '../../resources/images/works/man-city/image-2.png';
+import Manc3 from '../../resources/images/works/man-city/image-3.png';
+import Manc4 from '../../resources/images/works/man-city/image-4.png';
+import Manc5 from '../../resources/images/works/man-city/image-5.png';
+import Waves1 from '../../resources/images/works/the-waves/image-1.png';
+import Waves2 from '../../resources/images/works/the-waves/image-2.png';
+import Waves3 from '../../resources/images/works/the-waves/image-3.png';
+import Waves4 from '../../resources/images/works/the-waves/image-4.png';
+import Waves5 from '../../resources/images/works/the-waves/image-5.png';
+import Waves6 from '../../resources/images/works/the-waves/image-6.png';
+import Waves7 from '../../resources/images/works/the-waves/image-7.png';
+import Venue1 from '../../resources/images/works/the-venue/image-1.png';
+import Venue2 from '../../resources/images/works/the-venue/image-2.png';
+import Venue3 from '../../resources/images/works/the-venue/image-3.png';
+import Natours1 from '../../resources/images/works/natours/image-1.png';
+import Natours2 from '../../resources/images/works/natours/image-2.png';
+import Natours3 from '../../resources/images/works/natours/image-3.png';
+import Natours4 from '../../resources/images/works/natours/image-4.png';
+import Natours5 from '../../resources/images/works/natours/image-5.png';
+import Trillo1 from '../../resources/images/works/trillo/image-1.png';
+import Slack1 from '../../resources/images/works/slack-chat/image-1.png';
+import Slack2 from '../../resources/images/works/slack-chat/image-2.png';
+import Slack3 from '../../resources/images/works/slack-chat/image-3.png';
+import Shopping1 from '../../resources/images/works/shopping/image-1.png';
+import Shopping2 from '../../resources/images/works/shopping/image-2.png';
+import Shopping3 from '../../resources/images/works/shopping/image-3.png';
+import Shopping4 from '../../resources/images/works/shopping/image-4.png';
+import Shopping5 from '../../resources/images/works/shopping/image-5.png';
+import Shopping6 from '../../resources/images/works/shopping/image-6.png';
+
 export default [
     {
         name: 'man city',
-        url: "https://mcfc-joker7nbt.netlify.app/"
+        url: "https://mcfc-joker7nbt.netlify.app/",
+        mainPic: Manc1,
+        pictures: [Manc1, Manc2, Manc3, Manc4, Manc5]
     },
     {
         name: 'the waves',
-        url: "https://waves-joker7nbt.herokuapp.com/"
+        url: "https://waves-joker7nbt.herokuapp.com/",
+        mainPic: Waves1,
+        pictures: [Waves1, Waves2, Waves3, Waves4, Waves5, Waves6, Waves7]
     },
     {
         name: 'the venue',
-        url: "https://thevenue-joker7nbt.netlify.app/"
+        url: "https://thevenue-joker7nbt.netlify.app/",
+        mainPic: Venue1,
+        pictures: [Venue1, Venue2, Venue3]
     },
     {
         name: 'natours',
-        url: "https://natours-joker7nbt.netlify.app/"
+        url: "https://natours-joker7nbt.netlify.app/",
+        mainPic: Natours1,
+        pictures: [Natours1, Natours2, Natours3, Natours4, Natours5]
     },
     {
         name: 'trillo',
-        url: "https://trillo-joker7nbt.netlify.app/"
+        url: "https://trillo-joker7nbt.netlify.app/",
+        mainPic: Trillo1,
+        pictures: [Trillo1]
     },
     {
         name: 'slack chat',
-        url: "https://slackchat-joker7nbt.netlify.app/"
+        url: "https://slackchat-joker7nbt.netlify.app/",
+        mainPic: Slack1,
+        pictures: [Slack1, Slack2, Slack3]
     },
     {
         name: 'shopping',
-        url: "https://shoppingwebsite-template-joker7nbt.netlify.app/"
+        url: "https://shoppingwebsite-template-joker7nbt.netlify.app/",
+        mainPic: Shopping1,
+        pictures: [Shopping1, Shopping2, Shopping3, Shopping4, Shopping5, Shopping6]
     }
 ];
